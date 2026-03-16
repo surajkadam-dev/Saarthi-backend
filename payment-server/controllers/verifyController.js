d445f9e33e19de07013590cfa02f092bf4475476
Suraj Kadam <surajkadam1706004@gmail.com>
2026-03-17 02:10:26 +0530

    Update verifyController.js

--- payment-server/controllers/verifyController.js
+++ payment-server/controllers/verifyController.js
@@ -1,35 +1,46 @@
-const verifySignature = require("../utils/verifySignature.js");
- 
+const crypto = require("crypto");
+const db = require("../config/firebase");
+
 exports.verifyPayment = async (req, res) => {
- 
+
   try {
- 
+
     const {
       razorpay_order_id,
       razorpay_payment_id,
       razorpay_signature,
       parcelId
     } = req.body;
- 
-    const isValid = verifySignature(
-      razorpay_order_id,
-      razorpay_payment_id,
-      razorpay_signature
-    );
- 
-    if (!isValid) {
-      return res.status(400).json({ success: false });
+
+    const generatedSignature = crypto
+      .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
+      .update(razorpay_order_id + "|" + razorpay_payment_id)
+      .digest("hex");
+
+    if (generatedSignature !== razorpay_signature) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid signature"
+      });
     }
- 
-    // TODO: update Firestore payment status here
- 
+
+    await db.collection("parcels")
+      .doc(parcelId)
+      .update({
+        paymentStatus: "paid",
+        paymentId: razorpay_payment_id,
+        orderId: razorpay_order_id,
+        paidAt: new Date()
+      });
+
     res.json({
       success: true,
       paymentId: razorpay_payment_id
     });
- 
+
   } catch (error) {
+
     res.status(500).json({ error: error.message });
+
   }
- 
 };
