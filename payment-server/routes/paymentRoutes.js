9c50ef65de7b5f923dc545e466896a8e9ac1cfec
Suraj Kadam <surajkadam1706004@gmail.com>
2026-03-17 02:18:10 +0530

    Remove webhook route from paymentRoutes

--- payment-server/routes/paymentRoutes.js
+++ payment-server/routes/paymentRoutes.js
@@ -4,10 +4,10 @@ const router = express.Router();
  
 const { createOrder } = require("../controllers/orderController");
 const { verifyPayment } = require("../controllers/verifyController");
-const { razorpayWebhook } = require("../controllers/webhookController");
+
  
 router.post("/create-order", createOrder);
 router.post("/verify-payment", verifyPayment);
-router.post("/webhook", razorpayWebhook);
+
  
 module.exports = router;
